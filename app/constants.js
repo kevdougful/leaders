b74750d9cae01812bfbd86a84d07fde3e171c4d2
Kevin Coffey <kevdougful@gmail.com>
2015-10-12 23:57:26 -0500

    added constants and leaderboard controller

---
+++ app/constants.js
@@ -0,0 +1,18 @@
+app.constant('API', {
+  HOST: 'localhost',
+  TEAMS: {
+    GET_ALL: '/api/teams',
+    GET: '/api/team/',
+    CREATE: '/api/team/create',
+    UPDATE: '/api/team/update',
+    DELETE: '/api/team/delete'
+  },
+  PLAYERS: {
+    GET_ALL: '/api/players',
+    GET: '/api/player/',
+    CREATE: '/api/player/create',
+    UPDATE: '/api/player/update',
+    DELETE: '/api/player/delete'
+  },
+  LEADERS: '/api/leaders'
+})
