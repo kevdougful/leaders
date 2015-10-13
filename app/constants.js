1d3653a4c5fa7d8675d8a9b4a08035bfe2cd5ce0
Kevin Coffey <kdcoff1@monsanto.com>
2015-10-13 13:50:29 -0500

    fixed api route constants

--- app/constants.js
+++ app/constants.js
@@ -4,15 +4,15 @@ app.constant('API', {
     GET_ALL: '/api/teams',
     GET: '/api/team/',
     CREATE: '/api/team/create',
-    UPDATE: '/api/team/update',
-    DELETE: '/api/team/delete'
+    UPDATE: '/api/team/update/',
+    DELETE: '/api/team/delete/'
   },
   PLAYERS: {
     GET_ALL: '/api/players',
     GET: '/api/player/',
     CREATE: '/api/player/create',
-    UPDATE: '/api/player/update',
-    DELETE: '/api/player/delete'
+    UPDATE: '/api/player/update/',
+    DELETE: '/api/player/delete/'
   },
   LEADERS: '/api/leaders'
 });
