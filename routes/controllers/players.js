3c75a2f83d9304a4805cd7d00573d96d40460ca9
Kevin Coffey <kevdougful@gmail.com>
2015-10-13 20:58:07 -0500

    routes include associated objects

--- routes/controllers/players.js
+++ routes/controllers/players.js
@@ -2,7 +2,7 @@
 var respond = require('./utils').respond;
 
 module.exports = function(models) {
-	
+
 	var getAll = function(req, res, next) {
 		models.Player.findAll({
 			include: [
@@ -14,19 +14,22 @@ module.exports = function(models) {
 			respond(res, 500, false, err.message);
 		});
 	};
-	
+
 	var getById = function(req, res, next) {
 		models.Player.findOne({
 			where: {
 				id: req.params.player_id
-			}
+			},
+			include: [
+				models.Team
+			]
 		}).then(function(player) {
 			respond(res, 200, true, null, player);
 		}).catch(function(err) {
 			respond(res, 500, false, err.message);
 		});
 	};
-	
+
 	var createPlayer = function(req, res, next) {
 		models.Player.create({
 			Name: req.body.Name,
@@ -38,7 +41,7 @@ module.exports = function(models) {
 			respond(res, 500, false, err.message);
 		});
 	};
-	
+
 	var updatePlayer = function(req, res, next) {
 		models.Player.update({
 			Name: req.body.Name,
@@ -53,7 +56,7 @@ module.exports = function(models) {
 			respond(res, 500, false, err.message);
 		});
 	};
-	
+
 	return {
 		getAll: getAll,					// GET /api/players
 		getById: getById,				// GET /api/player/:sample_id
--- routes/controllers/teams.js
+++ routes/controllers/teams.js
@@ -2,29 +2,34 @@
 var respond = require('./utils').respond;
 
 module.exports = function(models) {
-	
+
 	var getAll = function(req, res, next) {
 		models.Team.findAll({
-			// where
+			include: [
+				models.Player
+			]
 		}).then(function(team) {
 			respond(res, 200, true, null, team);
 		}).catch(function(err) {
 			respond(res, 500, false, err.message);
 		});
 	};
-	
+
 	var getById = function(req, res, next) {
 		models.Team.findOne({
 			where: {
 				id: req.params.team_id
-			}
+			},
+			include: [
+				models.Player
+			]
 		}).then(function(team) {
 			respond(res, 200, true, null, team);
 		}).catch(function(err) {
 			respond(res, 500, false, err.message);
 		});
 	};
-	
+
 	var createTeam = function(req, res, next) {
 		models.Team.create({
 			Name: req.body.Name
@@ -34,7 +39,7 @@ module.exports = function(models) {
 			respond(res, 500, false, err.message);
 		});
 	};
-	
+
 	var updateTeam = function(req, res, next) {
 		models.Team.update({
 			Name: req.body.Name
@@ -48,7 +53,7 @@ module.exports = function(models) {
 			respond(res, 500, false, err.message);
 		});
 	};
-	
+
 	return {
 		getAll: getAll,				// GET /api/teams
 		getById: getById,			// GET /api/team/:sample_id
