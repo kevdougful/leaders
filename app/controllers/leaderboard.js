6e8295786aafedf1e8002103ccea9b794c9e7e4b
Kevin Coffey <kdcoff1@monsanto.com>
2015-10-14 13:29:09 -0500

    added leaderboard controller and view

--- app/controllers/leaderboard.js
+++ app/controllers/leaderboard.js
@@ -1,5 +1,29 @@
 'use strict';
 
-app.controller('LeaderboardController', function($scope, $http, $timeout, API) {
-  $scope.leaders
+app.controller('LeaderboardController', function($scope, $interval, LeaderboardService) {
+  LeaderboardService.getTeams().success(function(data) {
+    $scope.leaders = data.object;
+  });
+  LeaderboardService.getPlayers().success(function(data) {
+    $scope.players = data.object;
+  });
+  $scope.leaders = {};
+  $scope.players = {};
+  $scope.updated = Date.now();
+  $scope.stopLoop = function() {
+    $interval.cancel($scope.loop);
+  };
+  $scope.$on('$destroy', function() {
+    $scope.stopLoop();
+  });
+  $scope.loop = $interval(function() {
+    console.log('tick: ', Date.now());
+    LeaderboardService.getTeams().success(function(data) {
+      $scope.leaders = data.object;
+    });
+    LeaderboardService.getPlayers().success(function(data) {
+      $scope.players = data.object;
+    });
+    $scope.updated = Date.now();
+  }, 2000);
 });
